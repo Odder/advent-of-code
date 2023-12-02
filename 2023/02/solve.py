6f05d3c4ff3a012f85cb1cce40c20b0046d0a0b0
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2023-12-02 18:59:12 +0100

    :snowman: Refactored solution

--- 2023/02/solve.py
+++ 2023/02/solve.py
@@ -1,26 +1,19 @@
 from algas.input import tokens_filter
-from algas.aoc.aoc import part1, part2
+from algas.aoc.aoc import part1and2
 
 
-@part1(0)
-def solve1(total):
-    maxes = {'green': 13, 'blue': 14, 'red': 12}
+@part1and2(0, 0)
+def solve(t, p):
+    limits = {'green': 13, 'blue': 14, 'red': 12}       # Part 1
     for i, game in enumerate(tokens_filter(modifier=tuple, regex=r'(\d+) (red|green|blue)')):
+        maxes = {'green': 0, 'blue': 0, 'red': 0}       # Part 2
         for x, colour in game:
-            if int(x) > maxes[colour]:
-                i = -1
-        total += i + 1
-    return total
-
-
-@part2(0)
-def solve2(total):
-    for game in tokens_filter(modifier=tuple, regex=r'(\d+) (red|green|blue)'):
-        maxes = {'green': 0, 'blue': 0, 'red': 0}
-        for x, colour in game:
-            maxes[colour] = max(maxes[colour], int(x))
-        total += maxes['red'] * maxes['green'] * maxes['blue']
-    return total
+            maxes[colour] = max(maxes[colour], int(x))  # Part 2
+            if int(x) > limits[colour]:                 # Part 1
+                i = -1                                  # Part 1
+        t += i + 1                                      # Part 1
+        p += maxes['red'] * maxes['green'] * maxes['blue']  # Part 2
+    return t, p
 
 
 if __name__ == '__main__':
