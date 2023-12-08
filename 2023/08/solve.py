6909ea475e31031ba691b52f07b36ad4cee2139e
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2023-12-08 22:51:46 +0100

    :snowman: Day 8 - Refactored the sand storm

--- 2023/08/solve.py
+++ 2023/08/solve.py
@@ -1,15 +1,6 @@
 from algas.input import tokens_filter
 from algas.aoc.aoc import part1, part2
-import math
-from functools import reduce
-
-
-def lcm(a, b):
-    return a*b // math.gcd(a, b)
-
-
-def lcm_many(*args):
-    return reduce(lambda r, a: lcm(r, a), args, 1)
+from math import lcm
 
 
 @part2(lambda nw: [x for x in nw if x[-1] == 'A'], 0)
@@ -20,20 +11,19 @@ def solve2(start, n):
     network = {node: [l, r] for node, l, r in lines[2:]}
 
     currs = start(network)
-    seen = [{} for x in network if x[-1] == 'A']
+    seen = [{} for _ in currs]
     cycles = [0] * len(currs)
-    while any(curr[-1] != 'Z' for curr in currs):
+    while True:
         for i, curr in enumerate(currs):
             currs[i] = network[curr][dirs[n % len(dirs)]]
             if currs[i][-1] == 'Z':
-                if currs[i] in seen[i] and cycles[i] == 0 and (n - seen[i][currs[i]]) % len(dirs) == 0:
+                if currs[i] in seen[i] and cycles[i] == 0:
                     cycles[i] = n - seen[i][currs[i]]
 
                 seen[i][currs[i]] = n
                 if all(cycles):
-                    return lcm_many(*cycles)
+                    return lcm(*cycles)
         n += 1
-    return n
 
 
 if __name__ == '__main__':
