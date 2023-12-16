da2d4fe94d8859e356d27c25c106cf455e23edb0
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2023-12-16 11:48:11 +0100

    :snowman: Day 11 - Refactored

--- 2023/11/solve.py
+++ 2023/11/solve.py
@@ -1,38 +1,25 @@
 from algas.input import lines
 from algas.aoc.aoc import part1, part2
-from bisect import bisect_left
+from algas.spaces.s2 import traverse
+from bisect import bisect_left as bs
+from itertools import combinations
 
 
 @part2(1000000)
 @part1(2)
 def solve1(expansion):
-    grid = []
-    expansion_rows = []
-    expansion_cols = []
-    for i, line in enumerate(lines()):
-        grid.append(list(line))
-        if '#' not in line:
-            expansion_rows.append(i)
+    grid = [list(line) for line in lines()]
+    void_rows = [i for i, line in enumerate(grid) if '#' not in line]
+    void_cols = [j for j, line in enumerate(zip(*grid)) if '#' not in line]
+    galaxies = [(i, j) for i, j, cell in traverse(grid) if cell == '#']
+    dist = 0
+    voids = 0
 
-    for j in range(len(grid[0])):
-        for row in grid:
-            if row[j] == '#':
-                break
-        else:
-            expansion_cols.append(j)
+    for (i, j), (k, l) in combinations(galaxies, 2):
+        voids += abs(bs(void_rows, i) - bs(void_rows, k)) + abs(bs(void_cols, j) - bs(void_cols, l))
+        dist += abs(i-k) + abs(j-l)
 
-    dists = 0
-    seen = []
-    for i, row in enumerate(grid):
-        for j, cell in enumerate(row):
-            if cell == '#':
-                for seen_i, seen_j in seen:
-                    num_empty_rows = abs(bisect_left(expansion_rows, i) - bisect_left(expansion_rows, seen_i))
-                    num_empty_cols = abs(bisect_left(expansion_cols, j) - bisect_left(expansion_cols, seen_j))
-                    dists += abs(i-seen_i) + abs(j-seen_j) + (num_empty_rows + num_empty_cols) * (expansion-1)
-                seen.append((i, j))
-
-    return dists
+    return dist + voids * (expansion-1)
 
 
 if __name__ == '__main__':
